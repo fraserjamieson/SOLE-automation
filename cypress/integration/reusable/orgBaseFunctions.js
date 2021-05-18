645d512e11bff82d57d79bc24669482424a4fbd2
Alex Paterson <alex.paterson@gmail.com>
2021-05-18 19:56:47 +0100

    #59-admin-create-service

--- cypress/integration/reusable/orgBaseFunctions.js
+++ cypress/integration/reusable/orgBaseFunctions.js
@@ -1,12 +1,11 @@
-import solePg from "../pageObject/solePage";
-import admn from "../pageObject/cypMailPage";
+import solePg from '../pageObject/solePage';
+import admn from '../pageObject/cypMailPage';
 // let dataMap = new Map();
 class baseFunction {
   //   //Reusable methods
-  login(email) {
-    var pwd = Cypress.env("password");
+  login(email, pwd = Cypress.env('password')) {
     //lounch baseURL
-    cy.visit("/");
+    cy.visit('/');
     // cy.title().should("eq", "Local High Street");
     //Enter credentials
     solePg.email().type(email);
@@ -14,7 +13,7 @@ class baseFunction {
     //click login btn
     solePg.logInBtn().click();
     //assertion
-    solePg.adminMenu().should("be.visible");
+    solePg.adminMenu().should('be.visible');
     //solePg.setUpMenu().should("be.visible");
     //solePg.analyticsMenu().should("be.visible");
   }
@@ -23,13 +22,13 @@ class baseFunction {
     solePg.profileIcon().click();
     solePg.logoutBtn().click();
     //assertion
-    solePg.email().should("be.visible");
-    solePg.password().should("be.visible");
+    solePg.email().should('be.visible');
+    solePg.password().should('be.visible');
   }
   getRandomString(length) {
-    var result = "";
+    var result = '';
     var characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -37,8 +36,8 @@ class baseFunction {
     return result;
   }
   getRandomNumber(length) {
-    var result = "";
-    var characters = "0123456789";
+    var result = '';
+    var characters = '0123456789';
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -46,19 +45,19 @@ class baseFunction {
     return result;
   }
   getUniqueEmailID() {
-    var result = "automation";
+    var result = 'automation';
     var randomcharacters = this.getRandomString(6);
-    result = result + randomcharacters + "@test.com";
+    result = result + randomcharacters + '@test.com';
     return result;
   }
   clickOnElement(menuStr) {
     var navigateMenu = cy.get(menuStr);
-    navigateMenu.should("be.visible");
+    navigateMenu.should('be.visible');
     navigateMenu.click();
   }
   searchFirstRecord() {
     cy.wait(2000);
-    cy.get("table > tbody > tr:nth-child(1) > td:nth-child(1)").then(function (
+    cy.get('table > tbody > tr:nth-child(1) > td:nth-child(1)').then(function (
       $elem
     ) {
       returnText = $elem.text();
@@ -67,11 +66,11 @@ class baseFunction {
     });
   }
   mailLoging() {
-    var pwd = Cypress.env("mailpwd"),
-      mailName = Cypress.env("mail");
+    var pwd = Cypress.env('mailpwd'),
+      mailName = Cypress.env('mail');
 
     //cy.visit("https://login.aol.com/");
-    cy.visit("https://mail.aol.com/webmail-std/en-gb/suite");
+    cy.visit('https://mail.aol.com/webmail-std/en-gb/suite');
     admn.enterMailId(mailName);
     admn.enterMailPwd(pwd);
     // cy.get(".vp-cc-element.bottom.vp-hide",{ timeout: 20000 }).then(function ($style) {
