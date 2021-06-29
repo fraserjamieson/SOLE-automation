48fc56db01d6b92e9485534ea75dfc94a1f0e33c
Fraser Jamieson <fraserjamieson.93@gmail.com>
2021-06-29 20:08:59 +0100

    No address shown on org page asserted visible step 1

--- cypress/integration/soleSpecs/sole.org-03.js
+++ cypress/integration/soleSpecs/sole.org-03.js
@@ -1,12 +1,18 @@
 import basefunction from '../reusable/orgBaseFunctions';
 // import orgPg from "../pageObject/solePage";
+import cstBaseFunctions02 from '../reusable/cstBaseFunctions02';
+import customerBaseFunction from '../reusable/cstBaseFunctions01';
 
 describe('Customer user operations ', () => {
   const email = Cypress.env('existingEmail');
   const password = Cypress.env('existingEmailPwd');
+  const custEmail = Cypress.env('customerMail');
+  const custPW = Cypress.env('mailpwd');
 
   beforeEach(function () {
-    basefunction.login(email, password);
+    // basefunction.login(email, password);
+    cstBaseFunctions02.logInAsCustomer(custEmail);
+
   });
   xit('Org Admin can create a service', () => {
     // Visit services page
@@ -39,12 +45,60 @@ describe('Customer user operations ', () => {
     // Delete newly added service
     cy.get(':nth-child(1) > .justify-center > .v-icon').click();
   });
-  it("Org Admin can hide address", function () {
+  xit("Org Admin can hide address", function () {
     cy.contains('Edit Details').click();
     // Query for an element.
     // Interact with that element.
-     cy.get('.v-input--selection-controls__input').last().click();
+     cy.get('.v-input--selection-controls__input > input').last().click({force: true});
     // Assert about the content on the page.
     cy.get('.v-input--selection-controls__input > input').last().should('not.be.checked');
   });
+
+  it("Org address not showing on org page", function () {
+  // assert first li CHILDREN ELEMENT SHOULD BE VISIBLE AT FIRST
+
+    cy.visit('https://test.sole.scot/dunbar');
+    cy.get('.overlay-link .supplier-link').first().click();
+    cy.contains('Info').click();
+    // finding right-sidebar first post
+    // and the first descendant DOM li element
+    cy.get('.right-sidebar > .related-post > ul').find('li').first()
+    .should('be.visible');
+
+  })
+
+  xit('can show/hide address', () => {
+      // This assumes the address is shown by default
+  
+      cy.visit("/");
+      cy.adminLogin(existingEmail, existingEmailPwd);
+  
+      // Disables the address in the admin
+      cy.clickAddressCheckboxInAdmin();
+  
+      // Waits 5 seconds because of the iframe loading
+      cy.wait(5000);
+      // Checks the address doesn't show in the preview
+      // This will fail if the address doesn't contain Riverside
+      getIframeBody().find(".right-sidebar").should("not.contain", "Riverside");
+  
+      cy.visit(orgAddressURL);
+      cy.get(".right-sidebar").should("not.contain", "Riverside");
+  
+      cy.visit("/");
+      cy.adminLogin(existingEmail, existingEmailPwd);
+  
+      // Disables the address in the admin
+      cy.clickAddressCheckboxInAdmin();
+  
+      // Waits 5 seconds because of the iframe loading
+      cy.wait(5000);
+      // Checks the address doesn't show in the preview
+      // This will fail if the address doesn't contain Riverside
+      getIframeBody().find(".right-sidebar").should("contain", "Riverside");
+  
+      cy.visit(orgAddressURL);
+      cy.get(".right-sidebar").should("contain", "Riverside");
+  });
+  
 });
